ca019f27538e8c4b45489b5c54d6e4ed413862ce
James Robertson <jamesrobertsongittransfer@outlook.com>
2023-04-26 15:50:43 +0100

    experiments with adding daterange meta data and display on client

--- core/ffmpeg-experiments/ffmpeg.js
+++ core/ffmpeg-experiments/ffmpeg.js
@@ -8,6 +8,7 @@ const vidLocations = ['videos/input-test.mp4', 'videos/input-test-2.mp4'];
 const updateLivePlaylist = (vidLocation) => {
     return new Promise((resolve, reject) => {
         console.log('vidLocation', vidLocation)
+        console.log(new Date().toUTCString());
         ffmpeg()
             .addInput(vidLocation)
             .addOptions([
@@ -17,7 +18,7 @@ const updateLivePlaylist = (vidLocation) => {
                 '-hls_time 10',
                 '-hls_playlist_type event',
                 '-hls_list_size 0',
-                '-hls_flags append_list+omit_endlist',
+                '-hls_flags append_list+omit_endlist+program_date_time',
                 '-f hls'
 
             ]).output('videos/output.m3u8').on('end', () => {
