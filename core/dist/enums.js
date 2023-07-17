69e79cc15347f3b61c9ab7cf20d8e3e915ec7d82
James Robertson <jamesrobertson@Jamess-MacBook-Pro.local>
2023-07-17 20:10:00 +0100

    remove ffmpeg exps and dist folder

--- core/dist/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StreamStatus = void 0;
-var StreamStatus;
-(function (StreamStatus) {
-    StreamStatus["Started"] = "started";
-    StreamStatus["Finished"] = "finished";
-})(StreamStatus = exports.StreamStatus || (exports.StreamStatus = {}));
-//# sourceMappingURL=enums.js.map
